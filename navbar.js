508bb1cd3ddfb08425deaeb8fbcfcd3149dfe151
dsantos-emps <david.santos@eagles.oc.edu>
2024-09-14 00:02:40 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-fetch('http://project1.eaglesoftwareteam.com/project1/2024/t3/navbar/navbar.html')
+fetch('/project1/2024/t3/navbar/navbar.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('navbar-placeholder').innerHTML = data;
